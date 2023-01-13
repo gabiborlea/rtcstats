23685fcb83bb54ccace0a2d495aca67deaef0dd6
Gabriel Borlea <gabriel.borlea@8x8.com>
2023-01-13 12:00:15 +0200

    set max reconnect attemts

--- constants.js
+++ constants.js
@@ -1,5 +1,7 @@
 export const PROTOCOL_ITERATION = '3.1';
-export const MAX_RECONNECT_ATTEMPTS = 100;
+
+// this will allow the client to reconnect for maximum 8.5 minutes and maximum 11.5 minutes
+export const MAX_RECONNECT_ATTEMPTS = 8;
 export const messageTypes = {
     SequenceNumber: 'sn'
 };
